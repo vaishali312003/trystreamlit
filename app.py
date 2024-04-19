796576a8f971d4dc95d433c80f05f5b951a82511
vaishali312003 <162013691+vaishali312003@users.noreply.github.com>
2024-04-19 15:16:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 import streamlit as st
+from PIL import Image
+
+@st.cache
+def load_image(uploaded_file):
+    return Image.open(uploaded_file)
 
 def main():
     st.title("IMAGE UPLOAD HERE")
@@ -11,7 +16,12 @@ def main():
     
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
     if uploaded_file is not None:
-        st.image(uploaded_file, caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+        try:
+            image = load_image(uploaded_file)
+            st.image(image, caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+        except Exception as e:
+            st.error("Error: Unable to process the image. Please try a different file.")
+            st.write(e)
 
 if __name__ == '__main__':
     main()
