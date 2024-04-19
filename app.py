76baa92725495fb5cb1dfd439f79ef83b0fdb975
vaishali312003 <162013691+vaishali312003@users.noreply.github.com>
2024-04-19 12:55:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,21 @@
 import streamlit as st
+
+def main():
+    st.title("IMAGE UPLOADER")
+    html_temp = """
+    <div style="background-color:tomato;padding:10px">
+    <h2 style="color:white;text-align:center;">Streamlit Image uploader</h2>
+    </div>
+    """
+    st.markdown(html_temp, unsafe_allow_html=True)
+    
+    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+    if uploaded_file is not None:
+        st.image(uploaded_file, caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+
+if __name__ == '__main__':
+    main()
+import streamlit as st
 from streamlit_webrtc import webrtc_streamer, VideoProcessorBase, WebRtcMode
 pip install streamlit-webrtc
 
